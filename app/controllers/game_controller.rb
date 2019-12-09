97f4a142f7e1dfc9e4e5eac73f7d718d5ddb8fbe
033 <sp15sp@yandex.ru>
2019-12-09 13:02:36 +0700

    Add valera refresh

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,13 +6,14 @@ class GameController < ApplicationController
 
   def show;  end
 
-  def index
-    render :show
-  end
-
   def execute_action
     action = available.find { |a| a.name == params[:action_name] }
     valeroid = action.execute!(VALERA) unless action.nil?
+    redirect_to action: 'show'
+  end
+
+  def refresh
+  	VALERA.reinitialize!(100, 0, 0, 0, 100)
     redirect_to action: 'show'
   end
 end
