1685df73574e6f39b8ce127a2000998412a9f725
033 <sp15sp@yandex.ru>
2019-12-09 15:10:03 +0700

    delete trash

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,17 @@ ValeraModule::Application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  get 'game/index'
   get 'game/show'
   get 'game/load_valera'
   get 'game/save_valera'
   get 'game/init_valera'
 
-  resources :articles
   resources :posts do
     resources :comments
   end
   root 'welcome#index'
 
   get 'execute_action', to: 'game#execute_action', as: :execute_action
-
-  get 'refresh', to: 'game#refresh', as: :refresh
 
 end
 
