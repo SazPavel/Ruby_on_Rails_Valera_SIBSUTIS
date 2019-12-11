28beac79d64f89dea3919c3dd48bcf7201bfdb8b
033 <sp15sp@yandex.ru>
2019-12-11 12:47:04 +0700

    Add some translation

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ Bundler.require(*Rails.groups)
 
 module ValeraModule
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :ru
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Initialize configuration defaults for originally generated Rails version.
     #config.load_defaults 5.2
 
