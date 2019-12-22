9a0f7fe3f30e5a478ccb16e522da440512f80879
033 <sp15sp@yandex.ru>
2019-12-22 11:35:49 +0700

    Add valera_param validations

--- app/models/valera_param.rb
+++ app/models/valera_param.rb
@@ -1,2 +1,17 @@
 class ValeraParam < ApplicationRecord
+  validates :user_id, presence: true, numericality: true
+  validates :health, presence: true, 
+    		numericality: { greater_than_or_equal_to: 0,
+    			           less_than_or_equal_to: 100 }
+  validates :mana, presence: true, 
+    		numericality: { greater_than_or_equal_to: 0,
+    			           less_than_or_equal_to: 100 }
+  validates :cheerfulness, presence: true, 
+    		numericality: { greater_than_or_equal_to: -10,
+    			           less_than_or_equal_to: 10 }
+  validates :fatigue, presence: true, 
+    		numericality: { greater_than_or_equal_to: 0,
+    			           less_than_or_equal_to: 100 }
+  validates :money, presence: true, 
+    		numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/valera_param_spec.rb
+++ spec/models/valera_param_spec.rb
@@ -1,5 +1,94 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ValeraParam, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  describe 'when valid attributes' do
+    it 'is valid' do
+      expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+      					             cheerfulness: 10, fatigue: 100, 
+    	 				               money: 100)).to be_valid
+    end
+  end
+  describe 'when not valid attributes' do
+    context 'without attribute' do
+      it 'is not valid without user_id' do
+        expect(ValeraParam.new(health: 100, mana: 100, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid without health' do
+        expect(ValeraParam.new(user_id: 1, mana: 100, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid without mana' do
+        expect(ValeraParam.new(user_id: 1, health: 100,
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid without cheerfulness' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100,
+                               fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid without fatigue' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: 10, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid without money' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: 10, 
+                               fatigue: 100)).to_not be_valid
+      end
+    end
+    context 'with incorrect attribute' do
+      it 'is not valid if health less then 0' do
+        expect(ValeraParam.new(user_id: 1, health: -1, mana: 100, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if health less greater 100' do
+        expect(ValeraParam.new(user_id: 1, health: 101, mana: 100, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if mana less then 0' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: -1, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if mana less greater 100' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 101, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if cheerfulness less then -10' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: -11, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if cheerfulness less greater 10' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: 11, fatigue: 100, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if fatigue less then 0' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: 10, fatigue: -1, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if fatigue less greater 100' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: 10, fatigue: 101, 
+                               money: 100)).to_not be_valid
+      end
+      it 'is not valid if money less then 0' do
+        expect(ValeraParam.new(user_id: 1, health: 100, mana: 100, 
+                               cheerfulness: 10, fatigue: 100, 
+                               money: -1)).to_not be_valid
+      end
+    end
+  end
 end
